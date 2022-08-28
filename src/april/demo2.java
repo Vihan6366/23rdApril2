a5dd80b562d388eaad8c4be0a231d7f3491e01e5
vijay <vijay@LAPTOP-4IL3ACI3>
2022-08-28 17:18:46 +0530

    [vihan]:project 1 is success demo2

---
+++ src/april/demo2.java
@@ -0,0 +1,16 @@
+package april;
+
+public class demo2 
+{
+	public static void main(String[] args) 
+	{
+	System.out.println("Hallo");
+
+	System.out.println("Hallo");
+	
+	System.out.println("Hallo");
+	
+	System.out.println("Hallo");
+	}
+
+}
